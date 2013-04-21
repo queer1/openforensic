388f20aedbaba1d75001f10f7b01ea8548100205
Ben Barton <bbartontx@gmail.com>
2013-04-20 22:50:15 -0500

    first commit for new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openforensic::Application.config.secret_token = '03be8f6ff8c65e020e2bfe0fab1f20772132123f6f7edb21b2001237b1b5703774ab81965ac9f0f487d82a237f8c3e762bc78e731c4362d1d1f40ff8f7ecd212'
